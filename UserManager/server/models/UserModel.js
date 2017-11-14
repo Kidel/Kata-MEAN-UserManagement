53b9852c83b0e1e6f3bbb71236d305e3365a4ae8
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-11-14 23:13:06 +0100

    small edits to user model

--- UserManager/server/models/UserModel.js
+++ UserManager/server/models/UserModel.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var UserSchema = new Schema({
-    'email' : {type: String, required: true, unique: true},
-    'password': {type: String, required: true}, // TODO password needs to be hashed and salted
+    'email' : { type: String, required: true, unique: true },
+    'password': { type: String, required: true }, // TODO password needs to be hashed and salted
+    'salt': {type: String }, // TODO
     'created' : { type : Date, default: Date.now },
-    'status' : { type: Number } //0 = offline, 1 = idle, 2 = active. >=1 is online
+    'status' : { type: Number }, // 0 = offline, 1 = idle, 2 = active. >=1 is online
+    'auth' : { type : Number, default: 0 } 
 });
 
 module.exports = mongoose.model('User', UserSchema);
